57f19ad78a27a0a71252d78538c42e56c1ca7e08
Peter Mezei <mezeipetister@gmail.com>
2019-09-18 16:53:28 +0200

    init

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,20 @@
+// Copyright (C) 2019 Peter Mezei
+// 
+// This file is part of Project A.
+// 
+// Project A is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// Project A is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Project A.  If not, see <http://www.gnu.org/licenses/>.
+
 use super::{rocket, TemplateContext};
 
 use rocket::http::Method::*;
