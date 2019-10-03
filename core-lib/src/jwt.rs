6d3b69ea52f5f06759f15de9d5d0b011eab235b5
Peter Mezei <mezeipetister@gmail.com>
2019-10-03 16:04:02 +0200

    ...

--- core-lib/src/jwt.rs
+++ core-lib/src/jwt.rs
@@ -1,3 +1,53 @@
-pub fn jwt() {
-    println!("JWT!");
+use crypto::sha2::Sha256;
+use jsonwebtoken::{Header, Registered, Token};
+use std::default::Default;
+
+#[derive(Debug, Default, Serialize, Deserialize, PartialEq)]
+struct Claim {
+    username: String,
+    exp: usize,
+}
+
+impl Claim {
+    fn new() -> Claim {
+        Claim {
+            username: "username".to_string(),
+            exp: 100000000,
+        }
+    }
+    fn get_username(&self) -> String {
+        self.username.clone()
+    }
+}
+
+fn jwt_create_token(userid: String) -> Result<String, String> {
+    let header: Header = Default::default();
+    let claim = Registered {
+        iss: Some(userid),
+        ..Default::default()
+    };
+    let token = Token::new(header, claim);
+    Ok(token.signed(b"secret", Sha256::new()).unwrap())
+}
+
+fn jwt_decode_token(token: String) -> String {
+    let token = Token::<Header, Registered>::parse(&token).unwrap();
+    if (token.verify(b"secret", Sha256::new())) {
+        token.claims.iss.unwrap()
+    } else {
+        "".to_owned()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_jwt() {
+        let token = jwt_create_token("helloworld".to_owned()).unwrap();
+        let result = jwt_decode_token(token);
+        assert_eq!(result, "helloworld".to_owned());
+        assert_ne!(result, "asdasdad".to_owned());
+    }
 }
--- core-lib/src/lib.rs
+++ core-lib/src/lib.rs
@@ -1,3 +1,10 @@
+extern crate crypto;
+extern crate jwt as jsonwebtoken;
+#[macro_use]
+extern crate serde_derive;
+
 pub mod jwt;
 
-pub fn demo_function() {}
+pub fn demo_function() -> String {
+    "HelloWorld".to_string()
+}
