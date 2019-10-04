39ee7017edacf3db7606e27b755e945ff07483ad
Peter Mezei <mezeipetister@gmail.com>
2019-10-04 13:54:14 +0200

    ...

--- core/src/password.rs
+++ core/src/password.rs
@@ -1,7 +1,30 @@
+// Copyright (C) 2019 Peter Mezei
+//
+// This file is part of Project A.
+//
+// Project A is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Project A is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Project A.  If not, see <http://www.gnu.org/licenses/>.
+
 use bcrypt::{hash, verify};
 use rand::Rng;
-use std::char::ToUppercase;
 
+/// # Hash password
+/// ## Readme
+/// Get a password string pointer, returns a result<String, String>
+/// ```rust
+/// use core_lib::password::hash_password;
+/// let hash = hash_password(&"purple dog".to_owned()).unwrap();
+/// ```
 pub fn hash_password(password: &String) -> Result<String, String> {
     //let hashed = hash("hunter2", DEFAULT_COST)?;
     //let valid = verify("hunter2", &hashed)?;
@@ -27,13 +50,13 @@ pub fn generate_random_password(length: Option<u32>) -> Result<String, String> {
         .collect();
     // Generate random string, default length is 12
     for _ in 0..length.unwrap_or(12) {
-        // Gerenate random character
+        // Generate random character
         let random_ch = match chars.get(rng.gen_range(0, chars.len())) {
             Some(ch) => ch,
-            None => return Err("Error while generating random passowrd!".to_owned()),
+            None => return Err("Error while generating random password!".to_owned()),
         };
         // Random uppercase
-        let random_ch = match rng.gen_range(0, 1) {
+        let random_ch: char = match rng.gen_range(0, 1) {
             1 => random_ch.to_uppercase().nth(0).unwrap(),
             _ => random_ch.clone(),
         };
@@ -42,12 +65,13 @@ pub fn generate_random_password(length: Option<u32>) -> Result<String, String> {
     return Ok(password);
 }
 
+// Tests
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_hash_password() {
         use super::*;
-        let password = "loremipsumdolorem".to_owned();
+        let password = "purple_dog".to_owned();
         let hash = hash_password(&password).unwrap();
         assert_ne!(hash.len(), password.len());
     }
@@ -55,7 +79,7 @@ mod tests {
     #[test]
     fn test_verify_password() {
         use super::*;
-        let password = "loremipsumdolorem".to_owned();
+        let password = "purple_dog".to_owned();
         let hash = hash_password(&password).unwrap();
         assert_eq!(verify_password_from_hash(&password, &hash).unwrap(), true);
         assert_eq!(
