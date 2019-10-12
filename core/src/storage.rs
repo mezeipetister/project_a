09483a8f7c211595b319190fb5bbbed228bf0d03
Peter Mezei <mezeipetister@gmail.com>
2019-10-12 22:06:54 +0200

    object serialize + deserialize + unit tests added.

--- core/src/storage.rs
+++ core/src/storage.rs
@@ -15,6 +15,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Project A.  If not, see <http://www.gnu.org/licenses/>.
 
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
+
 /**
  * Storage DESIGN
  *
@@ -37,4 +40,53 @@
 pub fn load_storage<T>(path: &str) -> Result<Vec<T>, String> {
     let mut result: Vec<T> = Vec::new();
     Ok(result)
+}
+
+/**
+ * Serialize object<T> -> Result<String, String>
+ */
+pub fn serialize_object<T: Serialize>(object: &T) -> Result<String, String> {
+    match serde_yaml::to_string(object) {
+        Ok(result) => Ok(result),
+        Err(_) => Err("Error while data object serialisation.".to_owned()),
+    }
+}
+/**
+ * Deserialize &str into object<T>
+ */
+pub fn deserialize_object<'a, T: ?Sized>(s: &str) -> Result<T, String>
+where
+    for<'de> T: Deserialize<'de> + 'a,
+{
+    match serde_yaml::from_str(s) {
+        Ok(t) => Ok(t),
+        Err(_) => Err("Error while data object deserialization.".to_owned()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[derive(Serialize, Deserialize)]
+    struct Demo {
+        name: String,
+        age: u32,
+    }
+    #[test]
+    fn test_serialize_object() {
+        let d = Demo {
+            name: "Lorem Ipsum".to_owned(),
+            age: 99,
+        };
+        assert_eq!(
+            serialize_object(&d),
+            Ok("---\nname: Lorem Ipsum\nage: 99".to_owned())
+        );
+    }
+    #[test]
+    fn test_deserialize_object() {
+        let object: Demo = deserialize_object("---\nname: Lorem Ipsum\nage: 99").unwrap();
+        assert_eq!(object.name, "Lorem Ipsum".to_owned());
+        assert_eq!(object.age, 99);
+    }
 }
